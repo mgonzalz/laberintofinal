624835c9a25db6a970f212d27474b91a120fc14d
mgonzalz <mgonzgarc@myuax.com>
2022-12-08 20:19:05 +0100

    cambios

--- datos.py
+++ datos.py
@@ -9,4 +9,4 @@ laberinto = [
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3)) #Coordenadas de las X
 
 
-lo_que_debe_hacer = ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
+lo_que_debe_hacer = ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha']
