30d26c83796dca153a0f0948557434202f1e9afd
mgonzalz <mgonzgarc@myuax.com>
2022-12-08 19:52:33 +0100

    prueba

--- recorrer_laberinto.py
+++ recorrer_laberinto.py
@@ -7,24 +7,27 @@ from datos import *
     ['X', 'X', 'X', 'X', 'S']
 '''
 def recorrer_laberinto(laberinto):
-    # Fila y columnas iniciales
     m = 0
     n = 0
-    movimientos = ['Abajo'] # Movimiento inicial
-    tam = len(laberinto) # Tamaño del laberinto
-    while m < tam-1 and n < tam-1: # Mientras no llegue al final
-        if m + 1 < n and laberinto[m + 1][n] != 'X': # Movimiento anterior NO arriba; Fil+1 NO pared= ABAJO
-            m += 1 #Movimiento cuando es derecha
-            movimientos.append('Abajo')
-        elif m - 1 > 0 and laberinto[m - 1][n] != 'X':
-            m -= 1 #Movimiento cuando es arriba
-            movimientos.append('Arriba')
-        elif laberinto[m][n + 1] != 'X':
-            n += 1 #Movimiento cuando es derecha
-            movimientos.append('Derecha')
-        else:
-            n -= 1 #Movimiento cuando es izquierda
-            movimientos.append('Izquierda')
-    print(movimientos)
-if __name__ == '__main__':
-    recorrer_laberinto(laberinto)
+    tam = len(laberinto)
+    solucion = []
+    while (m < tam-1 and n < tam-1): #Que no llegue al final
+        if laberinto[m][n] == 'E':
+            solucion.append('Abajo')
+            m += 1 #Avanzar hacia abajo
+        elif laberinto[m][n] == ' ': #Si es un espacio
+            if laberinto[m][n+1] == ' ': #Misma fila, una columna a la derecha
+                solucion.append('Derecha')
+                n += 1 #Avanzar a la derecha
+            elif laberinto[m+1][n] == ' ': #Una fila abajo, misma columna
+                solucion.append('Abajo')
+                m += 1 #Avanzar hacia abajo
+            elif laberinto[m][n-1] == ' ': #Misma fila, una columna a la izquierda
+                solucion.append('Izquierda')
+                n -= 1 #Avanzar a la izquierda
+            else: #Una fila arriba, misma columna
+                solucion.append('Arriba')
+                m -= 1 #Avanzar hacia arriba
+    return solucion
+# Mostrar por pantalla la lista de movimientos
+print('Solución: ', recorrer_laberinto(laberinto))
